6098f99e50c42d2ed503092d8460dd8879d742c3
VanRar <vitalikf7@gmail.com>
2020-07-16 13:42:01 +0300

    процедурный подход, Создание вукнции Draw (вывода символа на кжран)

--- Snake123Resh/Snake123/Program.cs
+++ Snake123Resh/Snake123/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.CSharp.RuntimeBinder;
+using System;
 
 namespace Snake123
 {
@@ -6,8 +7,26 @@ namespace Snake123
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x1 = 1;
+            int y1 = 3;
+            char sym1 = '*';
 
+            Draw(x1, y1, sym1);
+
+            int x2 = 4;
+            int y2 = 5;
+            char sym2 = '#';
+
+            Draw(x2, y2, sym2);
+
+            Console.ReadLine();
+
+        }
+
+        static void Draw (int x, int y, char sym )
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
