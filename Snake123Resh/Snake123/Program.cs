6339d35e7d5f822208c80f3e83b8f0d391fb81d2
VanRar <vitalikf7@gmail.com>
2020-07-16 14:57:57 +0300

    Создали список значений инт в список при помощи List

--- Snake123Resh/Snake123/Program.cs
+++ Snake123Resh/Snake123/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.CSharp.RuntimeBinder;
 using System;
+using System.Collections.Generic;
 
 namespace Snake123
 {
@@ -12,7 +13,20 @@ namespace Snake123
             
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
-     
+
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
+
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
+
+            foreach(int i in numList)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
