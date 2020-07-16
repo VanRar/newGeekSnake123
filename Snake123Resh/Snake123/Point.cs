74acff34c1e672a4441ed18e80cfbb470aeaf202
VanRar <vitalikf7@gmail.com>
2020-07-16 18:24:20 +0300

    ещё жестче, змейка хомячит, частенько с перебоями

--- Snake123Resh/Snake123/Point.cs
+++ Snake123Resh/Snake123/Point.cs
@@ -48,6 +48,11 @@ namespace Snake123
                 y += offset;
             }
         }
+
+        public bool IsHit(Point p)
+        {
+            return p.x == this.x && p.y == this.y;
+        }
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
