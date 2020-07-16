427743b3d04f982ddd3d4b05f4e1300a81f1895d
VanRar <vitalikf7@gmail.com>
2020-07-16 13:27:45 +0300

    пробуем делать первую программу

---
+++ Snake123Resh/Snake123/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Snake123
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
